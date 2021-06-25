c824c4331965e9d72a8f3d81ee7d91f1a3598822
James <hello@jameshunt.com.au>
2021-06-25 13:51:46 +1000

    Refactor with stricter regex and non-primitive vars

--- src/index.js
+++ src/index.js
@@ -1,42 +1,37 @@
-const minDate = '01/01/1900';
-const maxDate = '31/12/2999';
+const minDate = '1900-01-01';
+const maxDate = '2999-12-31';
 
 const monthsInYear = 12;
 const daysInYear = 365;
 const daysInLeapYear = daysInYear+1;
 
-// Simple and allows for any separator
-// Also removes need to check isNan etc. later since input has to be 8 digits
-// Should we be more strict on separator?
-// Could use regex or even check string location of `/` if it needs to be strict
-const getDateString = (str) => {
-    const dateString = str.trim().replace(/[^\d]/g, '');
-    if(dateString.length !== 8){
-        throw new Error("Date is incorrect. Please enter date in format dd/mm/yyyy")
+// Returns date and dateInt, dateInt is to simplify comparison
+const dateRegex = /^\d{4}\-\d\d\-\d\d$/
+const getDateFromString = str => {
+    const dateString = str.trim();
+    if(!dateRegex.test(dateString)){
+        throw new Error("Incorrect date format. Please enter date matching yyyy-mm-dd")
     }
-    
-    return dateString
+
+    const date = dateString.split('-').map(str=>parseInt(str)); // [2020, 12, 25]
+    const dateInt = parseInt(dateString.replace(/-/g, '')); // 20201225
+
+    return [date, dateInt]
 }
 
-const getDay = dateString => parseInt(dateString.substr(0, 2));
-const getMonth = dateString => parseInt(dateString.substr(2, 2));
-const getYear = dateString => parseInt(dateString.substr(-4));
-
-// Make these dynamic in case we want to change the max/min constants later;
-const maxDateString = getDateString(maxDate);
-const minDateString = getDateString(minDate);
-const maxYear = getYear(maxDateString);
-const minYear = getYear(minDateString);
+const getDateIntFromString = (dateString) => dateString && dateRegex.test(dateString) && parseInt(dateString.replace(/-/g, ''))
+const maxDateInt = getDateIntFromString(maxDate);
+const minDateInt = getDateIntFromString(minDate);
 
 // Is a leap year if divisible by 4
 // Except if it's a century, then it must be divisible by 400
 // https://airandspace.si.edu/stories/editorial/science-leap-year
-const getIsLeapYear = (yearInt) => {
-    return yearInt % 100 === 0 ? yearInt % 400 === 0 : yearInt % 4 === 0
+const getIsLeapYear = (year) => {
+    return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0
 }
 
-const getDaysInMonth = (monthInt, isLeapYear) => {
-    switch(monthInt){
+const getDaysInMonth = (month, isLeapYear) => {
+    switch(month){
         case 4: 
         case 6:
         case 9:  
@@ -50,22 +45,23 @@ const getDaysInMonth = (monthInt, isLeapYear) => {
 }
 
 // Is X must be "between" confusing? Should it be "between x inclusive"?
-const validateDate = dateString => {
-    const year = getYear(dateString);
-    if(year > maxYear || year < minYear){
-        throw new Error(`Year must be between ${minYear}-${maxYear}`);
+const validateDate = (dateArray) => {
+    const [date, dateInt] = dateArray;
+
+    if((minDateInt && dateInt < minDateInt) || (maxDateInt && dateInt > maxDateInt)){
+        throw new Error(`Date must be between ${minDate} and ${maxDate}`);
     }
 
-    const month = getMonth(dateString);
+    const [ year, month, day ] = date;
+
     if(month < 1 || month > monthsInYear){
         throw new Error(`Month must be between 1-12`);
     }
 
     const isLeapYear = getIsLeapYear(year);
-    const day = getDay(dateString);
     const daysInMonth = getDaysInMonth(month, isLeapYear);
     if(day < 1 || day > daysInMonth){
-        throw new Error(`Date must be between 1-${daysInMonth} for ${dateString.substr(2,2)}/${year}`);
+        throw new Error(`Date must be between 1-${daysInMonth} for ${month}/${year}`);
     }
 }
 
@@ -73,10 +69,10 @@ const requestDateInput = (prompt) => new Promise(resolve => {
     process.stdout.write(prompt)
     process.stdin.on('data', data => {
         try{
-            const inputString = data.toString();
-            const dateString = getDateString(inputString);
-            validateDate(dateString);
-            resolve(dateString)
+            const dateString = data.toString();
+            const dateArray = getDateFromString(dateString);
+            validateDate(dateArray);
+            resolve(dateArray)
         }catch(e){
             console.warn('ERROR:', e.message);
             process.exit();
@@ -84,26 +80,13 @@ const requestDateInput = (prompt) => new Promise(resolve => {
     })
 })
 
-const getIsCorrectOrder = (a, b) => {
-    // Only continue comparison if same year
-    const y1 = getYear(a);
-    const y2 = getYear(b);
-    if(y1 !== y2){
-        return y1 < y2
-    }
+// Use simplified dateInteger eg. "20201025" to order
+// Return ordered dates
+const getOrderedDates = (input1, input2) => {
+    const [ date1, dateInt1] = input1;
+    const [ date2, dateInt2] = input2;
 
-    // Only continue comparison if same month
-    const m1 = getMonth(a);
-    const m2 = getMonth(b);
-    if(m1 !== m2){
-        return m1 < m2
-    }
-    
-    const d1 = getDay(a);
-    const d2 = getDay(b);
-    if(d1 !== d2){
-        return d1 < d2
-    }
+    return dateInt1 < dateInt2 ? [date1, date2] : [date2, date1]
 }
 
 const getDaysInYear = yearInt => getIsLeapYear(yearInt) ? daysInLeapYear : daysInYear
@@ -122,10 +105,8 @@ const getDaysBetweenYears = (y1, y2) => {
     return days
 }
 
-const getDayOfYear = dateString => {
-    const year = getYear(dateString);
-    const month = getMonth(dateString);
-    const day = getDay(dateString);
+const getDayOfYear = date => {
+    const [year, month, day] = date;
     const isLeapYear = getIsLeapYear(year);
 
     let dayOfYear = day;
@@ -138,21 +119,21 @@ const getDayOfYear = dateString => {
     return dayOfYear
 }
 
-const getDaysFromEndOfYear = dateString => {
-    const year = getYear(dateString);
+const getDaysFromEndOfYear = date => {
+    const [year] = date;
     const daysInThisYear = getDaysInYear(year);
-    const dayOfYear = getDayOfYear(dateString);
+    const dayOfYear = getDayOfYear(date);
     return daysInThisYear - dayOfYear;
 }
 
-const getDaysBetween = (a, b) => {
-    const y1 = getYear(a);
-    const y2 = getYear(b);
+const getDaysBetween = (date1, date2) => {
+    const [y1] = date1;
+    const [y2] = date2;
 
     const isSameYear = y1 === y2;
 
-    const doy1 = isSameYear ? getDayOfYear(a) : getDaysFromEndOfYear(a);
-    const doy2 = getDayOfYear(b);
+    const doy1 = isSameYear ? getDayOfYear(date1) : getDaysFromEndOfYear(date1);
+    const doy2 = getDayOfYear(date2);
 
     const daysBetweenYears = getDaysBetweenYears(y1, y2);
 
@@ -166,19 +147,15 @@ const getDaysBetween = (a, b) => {
 }
 
 const calculate = (input1, input2) => {
-    // Order dates to simplify calculation
-    const isCorrectOrder = getIsCorrectOrder(input1, input2);
-    const d1 = isCorrectOrder ? input1 : input2;
-    const d2 = isCorrectOrder ? input2 : input1;
-    return getDaysBetween(d1, d2);
+    const [ date1, date2 ] = getOrderedDates(input1, input2)
+    return getDaysBetween(date1, date2);
 }
 
 const main = async () => {
     // Get and validate inputs
-    const input1 = await requestDateInput("Enter first date (dd/mm/yyyy): ");
-    const input2 = await requestDateInput("Enter second date (dd/mm/yyyy): ");
-
-    const daysBetween = calculate(input1, input2);
+    const input1 = await requestDateInput("Enter first date (yyyy-mm-dd): ");
+    const input2 = await requestDateInput("Enter second date (yyyy-mm-dd): ");
+    const daysBetween = calculate(input1, input2)
 
     console.log(daysBetween);
     process.exit();
@@ -187,14 +164,5 @@ const main = async () => {
 module.exports = {
     main,
     calculate,
-    getDaysBetween,
-    getDaysFromEndOfYear,
-    getDayOfYear,
-    getDaysBetweenYears,
-    getDaysInYear,
-    getIsCorrectOrder,
-    validateDate,
-    getDaysInMonth,
-    getIsLeapYear,
-    getDateString
+    getDateFromString
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,22 @@
-const { calculate, getDateString } = require('./index');
+const { calculate, getDateFromString } = require('./index');
 
-test('calculates 02/06/1983 & 22/06/1983 to output 19', () => {
-    const date1 = getDateString('02/06/1983');
-    const date2 = getDateString('22/06/1983');
+test('calculates 1983-06-02 & 1983-06-22 to output 19', () => {
+    const date1 = getDateFromString('1983-06-02');
+    const date2 = getDateFromString('1983-06-22');
 
     expect(calculate(date1, date2)).toBe(19);
 });
 
-test('calculates 04/07/1984 & 25/12/1984 to output 173', () => {
-    const date1 = getDateString('04/07/1984');
-    const date2 = getDateString('25/12/1984');
+test('calculates 1984-07-04 & 1984-12-25 to output 173', () => {
+    const date1 = getDateFromString('1984-07-04');
+    const date2 = getDateFromString('1984-12-25');
 
     expect(calculate(date1, date2)).toBe(173);
 });
 
-test('calculates 03/01/1989 & 03/08/1983 to output 1979', () => {
-    const date1 = getDateString('03/01/1989');
-    const date2 = getDateString('03/08/1983');
+test('calculates 1989-01-03 & 1983-08-03 to output 1979', () => {
+    const date1 = getDateFromString('1989-01-03');
+    const date2 = getDateFromString('1983-08-03');
 
     expect(calculate(date1, date2)).toBe(1979);
 });
